d5a9282db22578b861e50241f1f5714959a71df2
YGourav <128379592+YGourav@users.noreply.github.com>
2023-07-29 00:04:04 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    'X-RapidAPI-Key': 'b1309be200msh1840d40bca602d7p1a8b0djsn29ddbd2147cb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "05bc52b08c2f1eef99d63ae79dd3dcba"; // enter your key from openweather API
